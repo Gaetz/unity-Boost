ee797633771676b0d199d2fdb07998cdeb12f4a1
Gaetz <gaetz@rufflerim.com>
2018-05-08 07:26:15 +0200

    Collision behaviour

---
+++ Assets/RocketCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter(Collision collision) {
+		switch(collision.gameObject.tag) {
+			case "Friendly":
+				// Do nothing
+				break;
+			case "Dangerous":
+				Destroy(gameObject);
+				break;
+		}
+	}
+}
