eb145cf9f18a583fa9e9ec34f8ba5045cd4e8013
Gaetz <gaetz@rufflerim.com>
2018-05-08 07:21:37 +0200

    Refactoring

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,54 +4,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
-	Rigidbody rb;
-
-	AudioSource thrustSource;
-
-	[SerializeField]
-	float rotationSpeed = 100f;
-
-	[SerializeField]
-	float thrustSpeed = 100f;
-
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody>();
-		thrustSource = GetComponent<AudioSource>();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		ProcessInput();
-	}
-
-	void ProcessInput() {
-		Thrust();
-		Rotate();
-	}
-
-	void Thrust() {
-		if(Input.GetKey(KeyCode.Space)) {
-			rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
-			if(!thrustSource.isPlaying) {
-				thrustSource.Play();
-			}
-		} else {
-			if(thrustSource.isPlaying) {
-				thrustSource.Stop();
-			}
-		}
-	}
-
-	void Rotate() {
-		rb.freezeRotation = true;
-		Vector3 rotation = Vector3.forward * Time.deltaTime * rotationSpeed;
-		if(Input.GetKey(KeyCode.Q)) {
-			transform.Rotate(rotation);
-		}
-		if(Input.GetKey(KeyCode.D)) {
-			transform.Rotate(-rotation);
-		}
-		rb.freezeRotation = false;
+		
 	}
 }
